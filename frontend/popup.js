cb219fb3726734803c48b03810f137ce3d02307f
netr212 <netra200021kcbdr@gmail.com>
2025-02-07 18:23:28 +0000

    Front end

---
+++ frontend/popup.js
@@ -0,0 +1,23 @@
+document.getElementById("sendRequest").addEventListener("click", async() => {
+    const randomText = ["This is awesome !", "Not good at all!"][Math.floor(Math.random() * 2)];
+
+    try{
+        const response = await fetch("httpt://localhost:5000/predict", {
+            method: "POST", 
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({
+                comments: [randomText]
+            })
+        });
+
+        const result = await response.json(); 
+        console.log("API Response: ", result); // Log API response to console.
+        document.getElementById("response").innnerText = JSON.stringify(result);
+    }
+    catch(error){
+        console.error("Error: ", error); // Log errors to console. 
+    }
+
+})
