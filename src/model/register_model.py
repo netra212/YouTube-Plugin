23f7f7b92025f3c5035b47b13eafa782d777000b
netr212 <netra200021kcbdr@gmail.com>
2025-04-25 13:29:48 +0100

    Registering model

--- src/model/register_model.py
+++ src/model/register_model.py
@@ -5,7 +5,7 @@ import mlflow
 import logging
 import os
 
-mlflow.set_tracking_uri("http://ec2-34-203-215-138.compute-1.amazonaws.com:5000")
+mlflow.set_tracking_uri("http://ec2-3-92-195-59.compute-1.amazonaws.com:5000/")
 
 # logging configuration
 logger = logging.getLogger('model_registration')
@@ -66,7 +66,7 @@ def main():
         model_info_path = 'experiment_info.json'
         model_info = load_model_info(model_info_path)
         
-        model_name = "yt_chrome_plugin_model"
+        model_name = "my_model"
         register_model(model_name, model_info)
     except Exception as e:
         logger.error('Failed to complete the model registration process: %s', e)
